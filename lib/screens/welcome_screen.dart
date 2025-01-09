56cb31aa03f8726c48ac8e7ee2a1cd402ffa9ad1
flutter <alkadafifirnawan1@gmail.com>
2025-01-09 15:26:26 +0700

    Tampilan-welcome

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+          Image.asset(
+            'assets/welcome.png',
+            width: 250,
+            height: 250,
+          ),
+          SizedBox(height: 30),
+          Text(
+            'APPELINE',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            // SizeBox(height: 10),
+            Text(
+              'Aplikasi Pembelajaran Online(APPELINE), membantu anda dalam belajar online',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    child: Text('Sign In'),
+                  ),
+                   SizedBox(width: 20),
+                   ElevatedButton(
+                    onPressed: () {
+
+                    },
+                    child: Text('Sign Up'),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+      ),
+    );
+  }
+}
